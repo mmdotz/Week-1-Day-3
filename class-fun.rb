2cae07490b6cb7b1b874d6f1f8e1b17d5401a533
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-08-26 12:00:12 -0500

    updated todays notes

--- class-fun.rb
+++ class-fun.rb
@@ -43,4 +43,42 @@ puts Student::CAMPUS #returns ATX
 
 #instance variables that have not been set are nil!!!!!
 
-jacob.wakes_up #does not fill in bed_time because it wasn't called
+jacob.wakes_up      #does not fill in bed_time because it wasn't called
+
+
+
+class Person
+  def initialize(name)        #this is never explicitly called
+    @name = name
+  end
+
+  def get_name                #need method to get instance variable out of class
+    @name.capitalize
+  end
+
+  def set_name(new_name)      #to change the name
+    @name = new_name
+end
+
+person1 = Person.new("bob") #don't name the variable the argument - causes confusion
+person2 = Person.new("jill")
+
+puts person1.name     #=>no method!
+puts person1.get_name   #=>bob
+puts person1.set_name("ben")   #ben
+
+student_records = Array.new
+student_records.push(Person.new('ryan'))
+student_records.push(Person.new('sam'))
+
+puts student_records #=>array
+
+student_records.each do |nstudent|     #=>each name on one line
+  puts nstudent.get_name
+end
+
+
+
+
+
+
